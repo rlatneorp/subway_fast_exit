f7fc98a8128be8c3f2b07c41f5e705b58243a2b7
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-11-18 05:31:05 +0900

    feat: ViewModel 및 LiveData gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 }
